0d15f201c108f9dcd30fcf8420b98025d2535dce
CRE-A <121703704+CRE-A@users.noreply.github.com>
2023-11-06 21:10:17 +0900

    update

--- project/src/main/java/project/service/UserDetailService.java
+++ project/src/main/java/project/service/UserDetailService.java
@@ -30,21 +30,27 @@ public class UserDetailService implements UserDetailsService {
         if (userINFO == null) {
             throw new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다. : " + id);
         }
-
         checkExpirationDate(userINFO);            //  계정만료일 지난 계정인지 체크
+
         return userINFO;
    }
 
 
 
+
     private void checkExpirationDate(UserDetailsDto userInfo) {
+
+        if(isRegisteredUser(userInfo)){                     // 회원가입 후 승인 대기중인 계정은, 빠져나감
+            return ;
+        }
+            System.out.println("1");
 
         Date now = new Date();
         Date expirationDate = userInfo.getExpirationDate();
 
         if (now.after(expirationDate)) {                     // 계정만료일을 지났으면, 계정 정지
             Map<String, Object> map = new HashMap();
-            map.put("enabled", 0);
+            map.put("enabled", 0);                           // (enabled =0) 으로 변경
             map.put("id", userInfo.getId());
 
             try {
@@ -53,6 +59,11 @@ public class UserDetailService implements UserDetailsService {
                 throw new RuntimeException(e);
             }
         }
+
+    }
+
+    private boolean isRegisteredUser(UserDetailsDto userINFO) {
+        return userINFO.getEnabled()== -1;
 
     }
 
